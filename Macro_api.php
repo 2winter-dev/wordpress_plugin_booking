a6c7588a57129c87def563964935a759302b394e
ywt_oi <winter_986@qq.com>
2022-09-09 18:06:02 +0800

    Macro插件：登录，用户接口

--- Macro_api.php
+++ Macro_api.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'BasicAuth.php';
 require_once 'Views.php';
+
 class Macro_api
 {
 
@@ -9,32 +9,45 @@ class Macro_api
     function __construct()
     {
 
-        add_action( 'admin_init', function (){self::booking_view_ext();} );
-        add_action( 'save_post',function ($post_id,$view){self::booking_cus_save($post_id,$view);}, 10, 2 );
-        add_filter('rest_authentication_errors', 'json_basic_auth_error');
-        add_filter('determine_current_user', 'json_basic_auth_handler', 20);
-        add_action('init', function () {self::booking_record_fun();});
-        add_action('manage_macro_booking_record_posts_custom_column',function ($column,$post_id){self::booking_admin_column($column,$post_id);},10,2);
-        add_filter('manage_macro_booking_record_posts_columns', function ($columns){return self::booking_admin_columns($columns);});
+        add_action('admin_init', function () {
+            self::booking_view_ext();
+        });
+        add_action('save_post', function ($post_id, $view) {
+            self::booking_cus_save($post_id, $view);
+        }, 10, 2);
+        add_action('rest_api_init', function () {
+            self::register_api();
+        });
+        add_action('init', function () {
+            self::booking_record_fun();
+        });
+        add_action('manage_macro_booking_record_posts_custom_column', function ($column, $post_id) {
+            self::booking_admin_column($column, $post_id);
+        }, 10, 2);
+        add_filter('manage_macro_booking_record_posts_columns', function ($columns) {
+            return self::booking_admin_columns($columns);
+        });
     }
 
 
-    private static function booking_admin_columns($columns){
+    private static function booking_admin_columns($columns)
+    {
         $columns['title'] = '预约课程|教练';
-        $columns['author'] ='顾客';
-        unset( $columns['date']);
+        $columns['author'] = '顾客';
+        unset($columns['date']);
         $columns['booking_time'] = '预约时间';
         $columns['booking_status'] = '预约状态';
         return $columns;
     }
 
-    private static function booking_admin_column($column, $post_id){
-        if($column == 'booking_status'){
-            $bg = get_post_meta( $post_id, 'booking_status', true ) == '0' ? 'tomato':'#578fff';
-            echo "<span style='background:$bg;padding:4px 10px;box-shadow: 0 1px 4px 2px rgba(0,0,0,0.1) ;cursor: pointer;border-radius: 15px;color: #fff'>" .(get_post_meta( $post_id, 'booking_status', true ) == '0' ? '未使用':'已使用').'</span>';
+    private static function booking_admin_column($column, $post_id)
+    {
+        if ($column == 'booking_status') {
+            $bg = get_post_meta($post_id, 'booking_status', true) == '0' ? 'tomato' : '#578fff';
+            echo "<span style='background:$bg;padding:4px 10px;box-shadow: 0 1px 4px 2px rgba(0,0,0,0.1) ;cursor: pointer;border-radius: 15px;color: #fff'>" . (get_post_meta($post_id, 'booking_status', true) == '0' ? '未使用' : '已使用') . '</span>';
         }
-        if($column == 'booking_time'){
-           echo str_replace('T',' ',get_post_meta( $post_id, 'booking_time', true ));
+        if ($column == 'booking_time') {
+            echo str_replace('T', ' ', get_post_meta($post_id, 'booking_time', true));
         }
 
     }
@@ -44,7 +57,6 @@ class Macro_api
         register_post_type('macro_booking_record',
             array(
                 'label' => '预约记录',
-                'map_meta_cap'=>true,
                 'labels' => array(
                     'name' => '课程预约',
                     'singular_name' => '课程预约清单',
@@ -59,11 +71,15 @@ class Macro_api
                     'not_found' => '没有找到',
                     'not_found_in_trash' => '没有发现'
                 ),
+
+
+                'show_ui' => true,
+                'show_in_menu' => true,
                 'public' => true,
-                'show_in_rest'=>true,
                 'description' => '预约管理',
                 'has_archive' => false,
-                'supports'=>[
+                'show_in_rest'=>false,
+                'supports' => [
                     'title',
                     'author'
                 ]
@@ -74,41 +90,86 @@ class Macro_api
     }
 
 
-    private static function booking_view_ext(){
-        add_meta_box( 'macro_review_meta_box',
-            '预约', function ($view){Macro_views::booking_cus_view($view);},
+    private static function booking_view_ext()
+    {
+        add_meta_box('macro_review_meta_box',
+            '预约', function ($view) {
+                Macro_views::booking_cus_view($view);
+            },
             'macro_booking_record', 'normal', 'high'
         );
 
     }
 
-    private static function booking_cus_save($post_id, $view ){
-        if ( $view->post_type == 'macro_booking_record' ) {
+    private static function booking_cus_save($post_id, $view)
+    {
+        if ($view->post_type == 'macro_booking_record') {
             // Store data in post meta table if present in post data
-            if ( isset( $_POST['booking_time'] ) && $_POST['booking_time'] != '' ) {
-                update_post_meta( $post_id, 'booking_time', $_POST['booking_time'] );
+            if (isset($_POST['booking_time']) && $_POST['booking_time'] != '') {
+                update_post_meta($post_id, 'booking_time', $_POST['booking_time']);
             }
-            if ( isset( $_POST['booking_status'] ) && $_POST['booking_status'] != '' ) {
-                update_post_meta( $post_id, 'booking_status', $_POST['booking_status'] );
+            if (isset($_POST['booking_status']) && $_POST['booking_status'] != '') {
+                update_post_meta($post_id, 'booking_status', $_POST['booking_status']);
             }
         }
     }
 
 
-
-
-
-    function macro_booking_query(): Macro_api
+    private static function register_api()
     {
-        return $this;
+
+
+        register_rest_route('macro', '/booking_signIn', array(
+            'methods' => 'GET',
+            'callback' => function ($request) {
+                return self::bookingSignIn($request);
+            },
+        ));
+        register_rest_route('macro', '/booking_list', array(
+            'methods' => 'GET',
+            'callback' => function ($request) {
+                return self::bookingListQuery($request);
+            },
+        ));
     }
 
-    function macro_booking_update(): Macro_api
+    private static function bookingListQuery($request): array
     {
-        return $this;
+        $cookie = $request->get_param('token');
+        $is_login = wp_validate_auth_cookie($cookie,'macro');
+        if($is_login){
+            $args = array(
+                'post_type'      => 'macro_booking_record',
+                'posts_per_page' => 10,
+            );
+            $data= (new WP_Query($args))->posts;
+            foreach ( $data as $key => $post ) {
+                $post->booking_status = get_post_meta( $post->ID, 'booking_status', true );
+                $post->booking_time = get_post_meta( $post->ID, 'booking_time', true );
+
+            }
+
+            return ['code'=>1,'msg'=>'SUCCESS','data'=>$data];
+        }else{
+            return ['code'=>-1,'msg'=>'请先登录','data'=>null];
+        }
+
 
     }
+
+
+    private static function bookingSignIn($request): array
+    {
+
+        $username =  $request->get_param('username');
+        $password = $request->get_param('password');
+        $user = wp_authenticate($username,$password);
+        return  ['code'=>1,'msg'=>'success','user'=>$user,'token'=>wp_generate_auth_cookie($user->ID,time()+7200,'macro')];
+
+    }
+
 
 
 }
+
 
--- Macro_booking.php
+++ Macro_booking.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Plugin Name:       Macro_booking
+ * Plugin Name:       MacroAPP
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Macro健身房APP用户预约接口
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            winter
- * Author URI:        /
+ * Author:            winter_986@qq.com
+ * Author URI:        http://blog.borebooks.top
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
@@ -18,12 +18,8 @@
 
 
 require_once 'Macro_api.php';
-
-
-
 $MACRO = new Macro_api();
 
-register_activation_hook(__FILE__, function (){});
 
 
 
